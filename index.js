02c7aecec62ee795519972c76989259e85373b0b
Md Fahad <fahad.programmer.me@outlook.com>
2025-05-11 13:47:46 +0600

    update cors

--- index.js
+++ index.js
@@ -30,7 +30,8 @@ const allowedOrigins = [
   'http://192.168.50.145:5173',
   'http://172.27.192.1:5173',
   'https://your-frontend.vercel.app',
-  'https://chat-app-web-client-81528t6wd-mdfahad80s-projects.vercel.app'
+  'https://chat-app-web-client-81528t6wd-mdfahad80s-projects.vercel.app',
+  'https://chat-app-web-client-git-main-mdfahad80s-projects.vercel.app'
 ];
 
 app.use(cors({
